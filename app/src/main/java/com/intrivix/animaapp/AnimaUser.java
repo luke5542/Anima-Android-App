d05943d5bc2dfe279d83589f0da03bbb7e01ea4f
Shyam Pinnipati <shamanshaman654@gmail.com>
2014-06-19 19:54:13 -0700

    added listview for stuffffff

---
+++ app/src/main/java/com/intrivix/animaapp/AnimaUser.java
@@ -0,0 +1,12 @@
+package com.intrivix.animaapp;
+
+/**
+ * Created by loneshaman on 6/19/2014.
+ */
+public class AnimaUser {
+    public int id;
+    public String name;
+    public String email;
+    public String time_zone;
+    public String authentication_token;
+}
